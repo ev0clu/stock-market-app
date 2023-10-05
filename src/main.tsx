9d34024da5c3a45b13e28f82904abcb2638853f1
ev0clu <ev0clu@gmail.com>
2023-10-05 16:33:09 +0200

    React route structure is added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App.tsx';
 import './assets/index.css';
+import SearchView from './views/SearchView.tsx';
+import DetailsView from './views/DetailsView.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<SearchView />} />
+      <Route path="/details" element={<DetailsView />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
