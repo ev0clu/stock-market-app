110b3941081036a1827c05135c77542ff69a319a
ev0clu <ev0clu@gmail.com>
2023-10-06 16:48:42 +0200

    DetailsList component is added, clean code

--- src/views/DetailsView.tsx
+++ src/views/DetailsView.tsx
@@ -1,24 +1,26 @@
 import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import DetailsList from '../components/DetailsView/DetailsList';
 
 function DetailsView() {
-  const { resetContext, stockCompanyData } = useContext(AppContext);
+  const { resetContext } = useContext(AppContext);
 
   useEffect(() => {
     resetContext();
   }, []);
 
   return (
-    <>
-      <div>DetailsView</div>
-      <pre>{JSON.stringify(stockCompanyData, null, 2)}</pre>
-      <Link to="/">
-        <div className="h-10 w-28 rounded bg-stone-500 px-2 py-2 text-white">
-          New Search
-        </div>
-      </Link>
-    </>
+    <div className="flex-1">
+      <DetailsList />
+      <div className="flex justify-center">
+        <Link to="/">
+          <div className="my-3 h-10 w-28 rounded bg-stone-500 px-2 py-2 text-center text-white">
+            New Search
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 }
 
