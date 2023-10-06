27e049f247ca907c40cc0002377a18a8d2a317ef
ev0clu <ev0clu@gmail.com>
2023-10-06 17:51:35 +0200

    Clean code

--- src/components/DetailsView/MetaData.tsx
+++ src/components/DetailsView/MetaData.tsx
@@ -4,26 +4,28 @@ import AppContext from '../../context/AppContext';
 function MetaData() {
   const { stockCompanyData } = useContext(AppContext);
 
+  const renderedData = Object.entries(
+    stockCompanyData['Meta Data']
+  ).map(
+    ([key, value], index) =>
+      index > 1 && (
+        <div
+          className="flex flex-row justify-between gap-2 border-b-2"
+          key={index}
+        >
+          <p>{key.slice(3, key.length)}</p>
+          <p>{value}</p>
+        </div>
+      )
+  );
+
   return (
     <>
       <h1 className="mx-10 my-5 flex flex-row gap-3 text-xl font-bold">
         {stockCompanyData['Meta Data']['1. Information']} (
         {stockCompanyData['Meta Data']['2. Symbol']})
       </h1>
-      <div className="mx-10 flex flex-col">
-        {Object.entries(stockCompanyData['Meta Data']).map(
-          ([key, value], index) =>
-            index > 1 && (
-              <div
-                className="flex flex-row justify-between gap-2 border-b-2"
-                key={index}
-              >
-                <p>{key.slice(3, key.length)}</p>
-                <p>{value}</p>
-              </div>
-            )
-        )}
-      </div>
+      <div className="mx-10 flex flex-col">{renderedData}</div>
     </>
   );
 }
