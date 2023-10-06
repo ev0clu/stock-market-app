1ba6659914a5ccbef4c62033ca092cb7c2adc00c
ev0clu <ev0clu@gmail.com>
2023-10-06 18:01:16 +0200

    Clean code

--- src/components/SearchView/SearchList.tsx
+++ src/components/SearchView/SearchList.tsx
@@ -5,6 +5,27 @@ function SearchList() {
   const { stockData, isLoading, stockError, handleDetails } =
     useContext(AppContext);
 
+  const renderData = stockData.bestMatches.map((stock, index) => (
+    <div
+      className="mx-10 flex flex-row items-center justify-between gap-4 border-b-2 py-2"
+      key={index}
+      data-symbol={stock['1. symbol']}
+    >
+      <div className="flex flex-row items-center gap-5">
+        <p className="font-bold">{stock['1. symbol']}</p>
+        <p className="break-word max-w-10rem whitespace-normal md:max-w-xl">
+          {stock['2. name']}
+        </p>
+      </div>
+      <button
+        className="h-7 w-7 rounded-full font-bold text-sky-700"
+        onClick={handleDetails}
+      >
+        Details
+      </button>
+    </div>
+  ));
+
   return (
     <>
       {isLoading ? (
@@ -18,26 +39,7 @@ function SearchList() {
               Stock has not found
             </p>
           ) : (
-            stockData.bestMatches.map((stock, index) => (
-              <div
-                className="mx-10 flex flex-row items-center justify-between gap-4 border-b-2 py-2"
-                key={index}
-                data-symbol={stock['1. symbol']}
-              >
-                <div className="flex flex-row items-center gap-5">
-                  <p className="font-bold">{stock['1. symbol']}</p>
-                  <p className="break-word max-w-10rem whitespace-normal md:max-w-xl">
-                    {stock['2. name']}
-                  </p>
-                </div>
-                <button
-                  className="h-7 w-7 rounded-full font-bold text-sky-700"
-                  onClick={handleDetails}
-                >
-                  Details
-                </button>
-              </div>
-            ))
+            renderData
           )}
         </div>
       )}
