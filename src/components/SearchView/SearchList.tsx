29611a67f8cd0a62dec94f82a0c08298a9642696
ev0clu <ev0clu@gmail.com>
2023-10-06 16:20:18 +0200

    SearchList design and loading handling is updated

--- src/components/SearchView/SearchList.tsx
+++ src/components/SearchView/SearchList.tsx
@@ -7,35 +7,40 @@ function SearchList() {
 
   return (
     <>
-      <div className="flex flex-col items-center">
-        {isLoading && <div>Loading ...</div>}
-      </div>
-      <div className="flex flex-col items-stretch">
-        {stockError ? (
-          <p className="mx-auto">Stock has not found</p>
-        ) : (
-          stockData.bestMatches.map((stock, index) => (
-            <div
-              className="mx-10 flex flex-row items-center justify-between gap-4 border-b-2 py-2"
-              key={index}
-              data-symbol={stock['1. symbol']}
-            >
-              <div className="flex flex-row items-center gap-5">
-                <p className="font-bold">{stock['1. symbol']}</p>
-                <p className="break-word max-w-10rem whitespace-normal md:max-w-xl">
-                  {stock['2. name']}
-                </p>
-              </div>
-              <button
-                className="h-7 w-7 rounded-full font-bold text-sky-700"
-                onClick={handleDetails}
+      {isLoading ? (
+        <div className="flex flex-col items-center">
+          <div>Loading ...</div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-stretch">
+          {stockError ? (
+            <p className="mx-auto my-5 text-red-600">
+              Stock has not found
+            </p>
+          ) : (
+            stockData.bestMatches.map((stock, index) => (
+              <div
+                className="mx-10 flex flex-row items-center justify-between gap-4 border-b-2 py-2"
+                key={index}
+                data-symbol={stock['1. symbol']}
               >
-                Details
-              </button>
-            </div>
-          ))
-        )}
-      </div>
+                <div className="flex flex-row items-center gap-5">
+                  <p className="font-bold">{stock['1. symbol']}</p>
+                  <p className="break-word max-w-10rem whitespace-normal md:max-w-xl">
+                    {stock['2. name']}
+                  </p>
+                </div>
+                <button
+                  className="h-7 w-7 rounded-full font-bold text-sky-700"
+                  onClick={handleDetails}
+                >
+                  Details
+                </button>
+              </div>
+            ))
+          )}
+        </div>
+      )}
     </>
   );
 }
