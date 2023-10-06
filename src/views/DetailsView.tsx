26b6612565c8d945f52369dc26b7e86f1df678a6
ev0clu <ev0clu@gmail.com>
2023-10-06 13:18:37 +0200

    Basic render element is added to the DetailsView component

--- src/views/DetailsView.tsx
+++ src/views/DetailsView.tsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 
 function DetailsView() {
-  const { resetContext } = useContext(AppContext);
+  const { resetContext, stockCompanyData } = useContext(AppContext);
 
   useEffect(() => {
     resetContext();
@@ -11,6 +12,12 @@ function DetailsView() {
   return (
     <>
       <div>DetailsView</div>
+      <pre>{JSON.stringify(stockCompanyData, null, 2)}</pre>
+      <Link to="/">
+        <div className="h-10 w-28 rounded bg-stone-500 px-2 py-2 text-white">
+          New Search
+        </div>
+      </Link>
     </>
   );
 }
