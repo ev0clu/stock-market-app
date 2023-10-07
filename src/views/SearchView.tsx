99191add92e262726b68b8219a58e73ba4d5a325
ev0clu <ev0clu@gmail.com>
2023-10-07 15:52:47 +0200

    Props reset handling is added to SearchView

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -1,7 +1,15 @@
+import { useContext, useEffect } from 'react';
+import AppContext from '../context/AppContext';
 import SearchBar from '../components/SearchView/SearchBar';
 import SearchList from '../components/SearchView/SearchList';
 
 function SearchView() {
+  const { resetContext } = useContext(AppContext);
+
+  useEffect(() => {
+    resetContext();
+  }, []);
+
   return (
     <div className="mx-10 flex-1">
       <SearchBar />
